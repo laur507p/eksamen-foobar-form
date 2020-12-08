e8f4d2585c21f7d56391583d67960cf372381d2c
Laura Toft <laur507p@stud.kea.dk>
2020-12-09 00:40:37 +0100

    basic struktur

--- src/components/Beers.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Beers() {
-  return <section className="screen" id="screen2"></section>;
-}
---
+++ src/components/pages/Beers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Beers() {
+  return (
+    <section className="screen" id="screen2">
+      <a href="#screen3" className="testbutton">
+        Next
+      </a>
+    </section>
+  );
+}
