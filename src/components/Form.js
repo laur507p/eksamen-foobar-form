f200d97ee6a2b6b73af2d7966d7b88ac36751209
Andrea Valgeirsdóttir <andreavalgeirsdottir@Andreas-MBP.lan>
2020-12-08 20:19:09 +0100

    form first commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,44 @@ import FormField from "./FormField";
 export default function Main() {
   return (
     <form className="form-container">
+      <h1>Customer information</h1>
+      <label>First Name</label>
       <FormField name="firstname" placeholder="First Name" type="text" id="firstname" />
+      
+      <label>Last name</label>
       <FormField name="lastname" placeholder="Last Name" type="text" id="lastname" />
-      <FormField name="telephone" placeholder="Telephone number" type="number" id="telephone" />
+      
+      <label>Email</label>
+      <div className="hint">A receipt will be sent to your email.</div>
+      <FormField name="email" placeholder="Email" type="text" id="email" />
+      
+      <label>Table number </label>
+      <div className="hint">You can find your tablenumber on the sign at the end of the table.</div>
+      {/* <FormField name="tablenumber" placeholder="Table number" type="number" id="tablenumber" /> */}
+      <select>
+        <option value="table1">1</option>
+        <option value="table2">2</option>
+        <option value="table3">3</option>
+        <option value="table4">4</option>
+        <option value="table5">5</option>
+        <option value="table6">6</option>
+        <option value="table7">7</option>
+        <option value="table8">8</option>
+        <option value="table9">9</option>
+        <option value="table10">10</option>
+      </select>
+
+      <h1>Payment information</h1>
+      <label>Card number</label>
+      <FormField name="cardnumber" placeholder="0000 0000 0000 0000" type="number" id="cardnumber"/>
+      
+      <label>mm/yy</label>
+      <FormField name="expiration_date" placeholder="12/20" type="number" id="expiration_date"/>
+
+      <label>cvc</label>
+      <FormField name="cvc" placeholder="111" type="number" id="cvc"/>
+     
     </form>
+    
   );
 }
