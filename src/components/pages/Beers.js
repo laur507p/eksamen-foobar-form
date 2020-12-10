3022e25ecf68d4252ac7f46b3ec3bacaf92fc64a
Andrea Valgeirsdóttir <andreavalgeirsdottir@Andreas-MBP.lan>
2020-12-10 15:27:39 +0100

    beers

--- src/components/pages/Beers.js
+++ src/components/pages/Beers.js
@@ -1,99 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
+import AddAndRemove from "../AddAndRemove";
+import GetTotal from "../GetTotal";
 
 
-      const beers = [
+function BeersList(){
+  
+    const Beers = [
         {
+          id: "1",
           name: "El Hefe",
           description: "The perfect beer",
           price: 65,
         },
         {
+          id: "2",
           name: "Ruined Childhood",
           description: "I cry",
           price: 60,
         },
         {
+          id: "3",
           name: "Githop",
           description: "Nerd alert",
           price: 65,
         },
-        {
+        { 
+          id: "4",
           name: "Mowintime",
           description: "Give me some soul sistah",
           price: 65,
         },
-       
-      
-      ]
-      
-      export default class Beers extends Component {
-      state = {
-        cart:[],
-      }
-      
-      add = (beer) => {
-        this.setState(state => ({
-          cart: [...state.cart, beer],
-        }))
-      }
-      
-      remove = (beer) => {
-        this.setState(state => {
-          const cart = [...state.cart];
-          const productIndex = cart.findIndex(b => b.name === beer.name);
-          if(productIndex < 0) {
-            return;
-          }
-          cart.splice(productIndex, 1)
-          return ({
-            cart
-          })
-        })
-      }
-      
-      currencyOptions = {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      }
-      
-      getTotal = () => {
-        const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price,0);
-        return total.toLocaleString(undefined, this.currencyOptions)
-      }
-      
-      render() {
-        return(
-          <section className="screen" id="screen2">
-              <a href="#screen3" className="testbutton">
-            Next
+      ];
+
+      return (
+        <section className="screen" id="screen2">
+            <a href="#screen3" className="testbutton">
+                Next
             </a>
-          <div className="wrapper">
-              <div>
-              Shopping Cart: {this.state.cart.length} total items.
-              </div>
-              <div>Total: {this.getTotal()}</div>
-      
-              <div>
-            {beers.map(beer => (
-              <div key={beer.name}>
-                <div className="beer">
-                  <p label={beer.name}>{beer.name}</p>
-                  <p label={beer.price}>{beer.price}</p>
-                </div>
-                <button onClick={() => this.remove(beer)}>-</button>
-                <span> 1 </span>
-                <button onClick={() => this.add(beer)}>+</button>
-                
-                
-              </div>
+            <ul>
+            {Beers.map(data =>(
+                <li key={data.id}>
+                <p>{data.name}</p>
+                <p>{data.price}</p>
+                <AddAndRemove price ={data.price}/>
+                </li>
             ))}
-          </div>
-          </div>
-          </section>
-          )
-       
-      }
-      }
-      
-      
-      
+            </ul>
+            <GetTotal/>
+             
+            
+      </section>
+
+      );
+}
+
+export default BeersList;
