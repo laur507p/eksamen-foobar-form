6b593041d597dc9fde704799236a2277f3889161
Markus <mark113c@stud.kea.dk>
2020-12-15 12:46:33 +0100

    Starte styling

---
+++ src/components/Window.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Window() {
+  return (
+    <div className="window-top">
+      <img src="window_elements.svg" className="window-elements" alt="Window buttons"></img>
+    </div>
+  );
+}
